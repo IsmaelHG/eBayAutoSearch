9eb74d875cd1e1e82ec5074edff371351a62ba9d
IsmaelHG <juegoseverywhereloquendo@gmail.com>
2021-01-30 16:02:36 +0100

    Example json file Telegram communication
    Parses all the ebay product id found on a single page search

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+import json
 import sys
 import requests
 from lxml import html
+import telebot
 
 
-def scrapper(url):
+def scrapper(file_name):
+    with open(file_name) as config_file:
+        config = json.load(config_file)
+        url = config["url"]
+        apikey = config["telegramAPIKEY"]
+        chatid = config["telegramCHATID"]
     r = requests.get(url)
     tree = html.fromstring(r.content)
-    productlist = [curr for curr in tree.xpath('//*[@id="ListViewInner"]//li[@listingid]')]
-    print(str(len(productlist))+" products found!")
-    for product in productlist:
-        print(product)
+    productlist = [curr.attrib["listingid"] for curr in tree.xpath('//*[@id="ListViewInner"]//li[@listingid]')]
+    print(str(len(productlist)) + " products found!")
+    idstr = '\n'.join(map(str, productlist))
+    print(idstr)
+    if apikey != "" and chatid != "":
+        telebot.TeleBot(apikey).send_message(chatid, idstr)
 
 
 if __name__ == '__main__':
