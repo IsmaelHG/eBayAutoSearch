da1d046697aded9e9820c4a438a1cbb32d973ac1
IsmaelHG <juegoseverywhereloquendo@gmail.com>
2021-01-31 12:01:33 +0100

    Big changes

--- README.md
+++ README.md
@@ -1,32 +1,45 @@
 # Basic eBay Scraper
 
-This is a pretty basic python script designed for getting every eBay product identifier from a specific search.
-The id list will be printed on console and also can be sent to a telegram account as a text message. This scraper only analyzes the first page of an eBay search, so it can only get the id of up to 200 items
+This is a pretty basic python script designed for getting every eBay listing link from a specific search.
+The url list will be printed on console and also can be sent to a telegram account as a text message. 
 
+It will search for new products every X seconds
 
 To execute this script, just run "python scraper.py config.json"
 
-config.json will be the path for a json file that contains the eBay search url, the telegram bot API Key 
+config.json will be the path for a json file that contains the eBay search url, database file location, sleep time, the telegram bot API Key 
 and the telegram chatid that is going to receive the message. This two previous parameters are 
 optional so you can just specify them as an empty string (""). Check the [example config](example.json) for reference
 
     eBay search URL can be get by just making a search with any filter you want (max price, location, only bids, etc)
     and copying the browser URL
-
+    
+    The databaseFile parameter contains the file path where it stores a database. 
+    This database will register every found listing so the program can only show (and send to telegram) new listings without duplicates
+    
+    The sleep parameter specifies a number of seconds. This is the amount of time that the scraper will wait for every search
+    
     Telegram API Key is retrieved when you create a new telegram bot on @BotFather
 
     The telegram chatid can be retrieved when you send a message to @userinfobot on telegram
 
+![alt text](ebaysearch2.jpg)
+
+![alt text](ebaysearch.jpg)
+
+
 ### Pre-requirements 📋
 
-You can quickly install all the requirements (lxml, requests, pyTelegramBotAPI) by using the well known 
+You can quickly install all the requirements by using the well known 
 
 ```
 pip install -r requirements.txt
 ```
 
-I'm developing this script for learning purposes so the script functionality may vastly change on the following weeks
+I'm developing this script for my own learning purposes so the script functionality may vastly change on the following weeks
 
 ### TODO
 * Make the script search for multiple pages
+* Remove old database entries
+* Show more detailed info about every listing (price, country, etc)
 
--- scraper.py
+++ scraper.py
@@ -1,12 +1,28 @@
 import json
 import sys
 import time
-
 import requests
-from lxml import html
 import telebot
 import sqlite3
+import re
+import datetime
+from urllib.parse import urlparse
+from lxml import html
 from sqlite3 import Error
+from signal import signal, SIGINT
+from sys import exit
+
+global con
+
+
+def exit_handler(signal_received, frame):
+    print("CTRL-C Pressed, exiting...")
+    try:
+        # Safely close the database connection and exit the application
+        con.close()
+    except Exception:
+        pass
+    exit(0)
 
 
 def sql_connection(file_name):
@@ -17,41 +33,58 @@ def sql_connection(file_name):
         print(Error)
 
 
-def scrapper(url, apikey, chatid, con, sleep):
+def scraper(url, apikey, chatid, sleep):
     cursor = con.cursor()
 
+    # Infinite loop, safe way to close the program is to send a SIGINT signal (CTRL-C)
     while True:
-        # Load and parse the html from the ebay search page
+        # Load and parse the html from supplied ebay search page
         r = requests.get(url)
         tree = html.fromstring(r.content)
 
-        # Get all the html elements that fulfill this xpath expression: //*[@id="ListViewInner"]//li[@listingid]
-        # and also parse the value from the field "listingid" to obtain every product id
-        productlist = [curr.attrib["listingid"] for curr in tree.xpath('//*[@id="ListViewInner"]//li[@listingid]')]
+        # Obtain every listing id
+        # eBay has two ways showing the listings, one is to show the listing with a "srp-results" class
+        # and another is by using "ListViewInner" class
+        if "srp-results" in r.text:
+            # srp-results
+            productlist = [re.findall("\d{12}", curr.attrib["href"])[0] for curr in
+                           tree.xpath('//*[contains(@class,"s-item__link")]')]
+        else:
+            # ListViewInner
+            productlist = [curr.attrib["listingid"] for curr in tree.xpath('//*[@id="ListViewInner"]//li[@listingid]')]
 
-        # Convert the list as a string and print it, every element is separated by a new-line
-        # save to database
+        # Insert every id into the database table
+        # If the id is already present on the table, cursor.execute() will raise an sqlite3.IntegrityError exception which will skip the process of sending the link
+        # as a telegram message
         for prodstr in productlist:
             try:
-                cursor.execute("INSERT INTO identifiers(id) VALUES(?)", (prodstr,))
-                print(prodstr)
-                # If the user specified a telegram bot apikey + chatid, it will send the previously printed list as a text message
+                # Insert the id and the timestamp
+                cursor.execute("INSERT INTO identifiers(id,listingDate) VALUES(?,?)",
+                               (prodstr, datetime.datetime.now()))
+
+                # Print the listing url based on the identifier
+                print("https://" + urlparse(url).netloc + "/itm/" + prodstr)
+
+                # If the user specified a telegram bot apikey + chatid, it will send the previously printed list as a text message (only if the previous line didn't produce an exception)
                 if apikey != "" and chatid != "":
                     try:
-                        telebot.TeleBot(apikey).send_message(chatid, "https://ebay.es/itm/" + prodstr)
-                        time.sleep(0.3)
+                        telebot.TeleBot(apikey).send_message(chatid,
+                                                             "https://" + urlparse(url).netloc + "/itm/" + prodstr)
+                        # Telegram API limits the number of messages per second so we need to wait a little bit
+                        time.sleep(0.5)
                     except telebot.apihelper.ApiTelegramException:
                         pass
             except sqlite3.IntegrityError:
+                # When this exception rises, the program will just continue to the next element of the for-loop
                 pass
         con.commit()
+        # Wait before repeting the process
         time.sleep(int(sleep))
-    con.close()
 
 
 if __name__ == '__main__':
     # Obtain parameters from the json file
-    # User must specify the file path as the first argument when running this script
+    # User must specify the file path as an argument when running this script
     with open(sys.argv[1]) as config_file:
         config = json.load(config_file)
         url = config["url"]
@@ -60,10 +93,14 @@ if __name__ == '__main__':
         dbname = config["databaseFile"]
         sleep = config["sleep"]
 
-    # Connect to db
+    # Connect to db and create the table (if not exists)
     con = sql_connection(dbname)
     cursor = con.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS identifiers(id VARCHAR(12) PRIMARY KEY)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS identifiers(id VARCHAR(12) PRIMARY KEY, listingDate timestamp)")
     con.commit()
 
-    scrapper(url, apikey, chatid, con, sleep)
+    # Start the exit handler
+    signal(SIGINT, exit_handler)
+
+    # Start the scraper
+    scraper(url, apikey, chatid, sleep)
